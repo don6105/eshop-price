d792ac9cc268c773dd0af2b2c94442cf554cdb41
undersky <don0910129285@gmail.com>
2021-07-05 19:21:13 +0800

    add dynamic relationship between summary and game.
    read app/models/Game[A-Z]{2} and assign to Relation::morphMap().

--- app/Models/Summary.php
+++ app/Models/Summary.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Cache;
 use App\Models\Base as Model;
 
 class Summary extends Model
@@ -15,4 +17,32 @@ class Summary extends Model
     protected $table      = 'summary';
     protected $primaryKey = 'ID';
     protected $guarded    = ['ID'];
+
+    public function game()
+    {
+        $relationships = $this->getRelationWithGame();
+        Relation::morphMap($relationships);
+        return $this->morphTo(__FUNCTION__, 'Country', 'GameID');
+    }
+
+    private function getRelationWithGame()
+    {
+        // get dynamic relationship by reading path of models
+        // ex. ['us' => 'App\Models\GameUs', ... ]
+        $cache = Cache::store('file');
+        if ($cache->has(__FUNCTION__)) {
+            return $cache->get(__FUNCTION__);
+        }
+        $relationships = [];
+        foreach (scandir(app_path('Models')) as $model) {
+            if (strpos($model, 'Game') === false) { continue; }
+            if (preg_match('/Game([A-Za-z]{2})/', $model, $m) > 0) {
+                $country = strtolower($m[1]);
+                $model   = 'App\\Models\\Game'.ucfirst($country);
+                $relationships[ $country ] = $model;
+            }
+        }
+        $cache->put(__FUNCTION__, $relationships, now()->addHours(12));
+        return $relationships;
+    }
 }
